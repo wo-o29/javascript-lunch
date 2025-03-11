f925350b82afeb4bedac23e45802851c37aea081
wo-o29 <woogur29@gmail.com>
2025-03-11 16:22:00 +0900

    refactor: 타입 수정

--- src/types/type.ts
+++ src/types/type.ts
@@ -8,9 +8,9 @@ export type Category = "한식" | "중식" | "일식" | "양식" | "아시안" |
 
 export type CategoryFilter = "전체" | Category;
 
-export type SortFilter = "이름순" | "거리순";
+export type SortedOption = "name" | "distance";
 
-export type Distance = "5" | "10" | "15" | "20" | "30";
+export type Distance = 5 | 10 | 15 | 20 | 30;
 
 export interface Restaurant {
   category: Category;
