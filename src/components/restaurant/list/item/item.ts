3bcd0f928aeed25f647482434957bb3da48a080e
wo-o29 <woogur29@gmail.com>
2025-03-11 21:24:12 +0900

    refactor: RestaurantItem 파일 위치 수정(list 내부로 이동)

--- src/components/restaurant/list/item/item.ts
+++ src/components/restaurant/list/item/item.ts
@@ -1,4 +1,8 @@
-import type { Restaurant } from "../../../types/type.ts";
+import { STORAGE_KEY } from "../../../../settings/settings.ts";
+import type { Restaurant } from "../../../../types/type.ts";
+import { setStorage } from "../../../../utils/storage.ts";
+import createStarIcon from "../../../starIcon/starIcon.ts";
+import { getRestaurantList } from "../restaurantList.ts";
 
 const categoryIcon = {
   한식: "./category-korean.png",
@@ -9,11 +13,37 @@ const categoryIcon = {
   기타: "./category-etc.png",
 };
 
+function replaceStarIcon({ isFavorite }: Record<string, boolean>) {
+  document.querySelector(".star-icon")?.replaceWith(
+    createStarIcon({
+      isFill: isFavorite,
+    })
+  );
+}
+
+function handleRestaurantItemClick(e: MouseEvent, id: string) {
+  const target = e.target as HTMLElement;
+  if (target.closest(".star-icon")) {
+    const restaurantList = getRestaurantList().map((restaurant) => {
+      if (restaurant.id === id) {
+        restaurant.isFavorite = !restaurant.isFavorite;
+        replaceStarIcon({ isFavorite: restaurant.isFavorite });
+      }
+
+      return restaurant;
+    });
+
+    setStorage(STORAGE_KEY.RESTAURANTS, restaurantList);
+  }
+}
+
 export default function createRestaurantItem({
+  id,
   category,
   name,
   distance,
   description,
+  isFavorite,
 }: Restaurant) {
   const restaurantItem = createElement("li", { className: "restaurant" });
 
@@ -42,6 +72,13 @@ export default function createRestaurantItem({
   });
   infoBox.append(nameElement, distanceElement, descriptionElement);
 
-  restaurantItem.append(categoryBox, infoBox);
+  restaurantItem.append(
+    categoryBox,
+    infoBox,
+    createStarIcon({ isFill: isFavorite })
+  );
+  restaurantItem.addEventListener("click", (e: MouseEvent) =>
+    handleRestaurantItemClick(e, id)
+  );
   return restaurantItem;
 }
