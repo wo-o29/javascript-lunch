57b41e346ad4cd3b6f341138cee79224017f1fc7
wo-o29 <woogur29@gmail.com>
2025-03-12 20:15:34 +0900

    refactor: 모달 로직 수정

--- src/components/bottomSheet/bottomSheet.ts
+++ src/components/bottomSheet/bottomSheet.ts
@@ -1,36 +1,47 @@
+import type { Restaurant } from "../../types/type";
 import { getElement } from "../../utils/dom";
+import createRestaurantDetail from "../restaurant/detail/restaurantDetail";
 import createRestaurantForm from "../restaurant/form/form";
 
 function bottomSheetController() {
-  let isFirstRender = false;
   const modal = getElement(".modal") as HTMLDialogElement;
+  const modalContainer = getElement(".modal-container");
 
   function handleModalClose() {
     modal.close();
   }
 
+  function openRestaurantDetailModal(RestaurantInfo: Restaurant) {
+    modal.showModal();
+    modalContainer.replaceChildren(createRestaurantDetail(RestaurantInfo));
+  }
+
+  function openRestaurantAddFormModal() {
+    modal.showModal();
+    modalContainer.replaceChildren(createRestaurantForm());
+  }
+
   function handleBottomSheetToggle(event: MouseEvent) {
     const target = event.target as HTMLElement;
 
-    if (target?.closest(".restaurant-add-button")) {
-      modal.showModal();
-
-      if (!isFirstRender) {
-        const modalContainer = getElement(".modal-container");
-        const restaurantFrom = createRestaurantForm();
-
-        modalContainer.appendChild(restaurantFrom);
-        isFirstRender = true;
-      }
+    if (target.closest(".restaurant-add-button")) {
+      openRestaurantAddFormModal();
     }
 
-    if (target?.closest(".modal-backdrop")) {
+    if (target.closest(".modal-backdrop")) {
       modal.close();
     }
   }
 
-  return { handleModalClose, handleBottomSheetToggle };
+  return {
+    handleModalClose,
+    handleBottomSheetToggle,
+    openRestaurantDetailModal,
+  };
 }
 
-export const { handleModalClose, handleBottomSheetToggle } =
-  bottomSheetController();
+export const {
+  handleModalClose,
+  handleBottomSheetToggle,
+  openRestaurantDetailModal,
+} = bottomSheetController();
